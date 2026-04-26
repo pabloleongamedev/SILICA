67af8e19b1b150d03fc0eab08847f86ed5fa7afd
FRANCISCO <pachonetworking@gmail.com>
2026-04-26 17:23:36 -0500

    Tecla E no funciona para el teletransporte

---
+++ Assets/Project/Scripts/Systems/TeleportManager/TeleportManager.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TeleportManager : MonoBehaviour
+{
+    public InputActionReference teleportAction;
+    public Transform spaceShipHall;
+    public Transform exteriorMap;
+    public GameObject player;
+    public GameObject visualPrompt;
+    private bool onSpaceShip = false;
+    private bool isNearDoor = false;
+
+    private void OnEnable()
+    {
+        teleportAction.action.Enable();
+    }
+    private void OnDisable()
+    {
+        teleportAction.action.Disable();
+    }
+
+
+    void Start()
+    {
+        if (visualPrompt != null)
+        {
+            visualPrompt.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearDoor && teleportAction.action.triggered)
+        {
+            teleportPlayer();
+        }
+    }
+
+
+    private void teleportPlayer()
+    {
+        Vector3 endPoint = onSpaceShip ? exteriorMap.position : spaceShipHall.position;
+        CharacterController controller = player.GetComponent<CharacterController>();
+        Rigidbody rigidBody = player.GetComponent<Rigidbody>();
+        if (controller != null)
+        {
+            controller.enabled = false;
+        }
+        player.transform.position = endPoint;
+        if (rigidBody != null)
+        {
+            rigidBody.linearVelocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+            rigidBody.isKinematic = true;
+        }
+        
+        player.transform.position = endPoint;
+        if (controller != null) controller.enabled = true;
+
+        if (rigidBody != null)
+        {
+            rigidBody.isKinematic = false;
+        }
+        onSpaceShip = !onSpaceShip;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = true;
+            Debug.Log("Tecla E");
+            if (visualPrompt != null)
+            {
+                visualPrompt.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = false;
+            if (visualPrompt != null)
+            {
+                visualPrompt.SetActive(false);
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
