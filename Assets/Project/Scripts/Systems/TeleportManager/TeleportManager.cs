ce5b173f6d7cbb5368f947cb708f7ae17ef3352c
FRANCISCO <pachonetworking@gmail.com>
2026-04-27 14:06:41 -0500

    Teletransporte al interior de la nave

--- Assets/Project/Scripts/Systems/TeleportManager/TeleportManager.cs
+++ Assets/Project/Scripts/Systems/TeleportManager/TeleportManager.cs
@@ -3,26 +3,90 @@ using UnityEngine.InputSystem;
 
 public class TeleportManager : MonoBehaviour
 {
-    public InputActionReference interactAction;
-    private void OnEnable() => interactAction.action.Enable();
-    private void OnDisable() => interactAction.action.Disable();
+    public InputActionReference teleportAction;
+    public Transform spaceShipHall;
+    public Transform exteriorMap;
+    public GameObject player;
+    public GameObject visualPrompt;
+    private static bool onSpaceShip = false;
+    private bool isNearDoor = false;
 
-    void Update()
+    private void OnEnable()
     {
-        if (interactAction.action.triggered)
-        {
-            Debug.Log($"Log: Tecla E Puldsada");
-        }
+        teleportAction.action.Enable();
     }
+    private void OnDisable()
+    {
+        teleportAction.action.Disable();
+    }
+
 
     void Start()
     {
-        // Esto imprimirá cada acción y la tecla que tiene asignada
-        foreach (var action in interactAction.action.actionMap.actions)
+        if (visualPrompt != null)
         {
-            foreach (var binding in action.bindings)
+            visualPrompt.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearDoor && teleportAction.action.triggered)
+        {
+            Debug.Log("¡BOTÓN PRESIONADO! Iniciando teletransporte...");
+            teleportPlayer();
+        }
+    }
+
+
+    private void teleportPlayer()
+    {
+        Vector3 endPoint = onSpaceShip ? exteriorMap.position : spaceShipHall.position;
+        CharacterController controller = player.GetComponent<CharacterController>();
+        Rigidbody rigidBody = player.GetComponent<Rigidbody>();
+        if (controller != null)
+        {
+            controller.enabled = false;
+        }
+        if (rigidBody != null)
+        {
+            rigidBody.linearVelocity = Vector3.zero;
+            rigidBody.isKinematic = true;
+        }
+        player.transform.position = endPoint;
+        Physics.SyncTransforms();
+        if (controller != null) controller.enabled = true;
+
+        if (rigidBody != null)
+        {
+            rigidBody.isKinematic = false;
+        }
+        onSpaceShip = !onSpaceShip;
+        isNearDoor = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = true;
+            Debug.Log("Tecla E");
+            if (visualPrompt != null)
             {
-                Debug.Log($"Acción: {action.name} | Tecla: {binding.path}");
+                visualPrompt.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = false;
+            if (visualPrompt != null)
+            {
+                visualPrompt.SetActive(false);
             }
         }
     }
